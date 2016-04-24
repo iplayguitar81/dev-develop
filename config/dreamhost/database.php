2b5c0c9ec06d0294aa8fde8ede78572f56685236
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-24 02:00:03 -0700

    get this stuff figured out

--- config/dreamhost/database.php
+++ config/dreamhost/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('db-laravel.bowtiesoft.com', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('regisridge', 'forge'),
+            'username' => env('bowtiesoft', 'forge'),
+            'password' => env('Drexler22', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
