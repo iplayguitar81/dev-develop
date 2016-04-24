085ec35edfa5da90e3a7cc2d3d5801d266801596
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-24 02:38:00 -0700

    get this stuff figured out

--- config/dreamhost/database.php
+++ config/dreamhost/database.php
@@ -51,6 +51,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
+        
 
         'mysql' => [
             'driver' => 'mysql',
