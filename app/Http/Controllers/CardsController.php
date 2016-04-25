a3247b350e8cf255c9dbb6f8014bba41ac3b2168
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-24 23:39:42 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Card;
 use Illuminate\Http\Request;
 use DB;
 use App\Http\Requests;
@@ -12,12 +12,33 @@ class CardsController extends Controller
     //
     public function index(){
 
-        //$cards= \DB::table('cards')->get();
-        $cards = DB::connection('mysql')
-            ->table('cards')
-            ->get();
+//        //$cards= \DB::table('cards')->get();
+//        $cards = DB::connection('mysql')
+//            ->table('cards')
+//            ->get();
+//
+        //ELOQUIN
+        $cards =Card::all();
+       return view('cards.index',compact('cards'));
 
-        return view('cards.index',compact('cards'));
+
     }
+
+    public function show(Card $card){
+
+        //ELOQUIN???
+
+     //   $card = Card::find($id);
+
+        //spits out JSON version to see specific JSON object
+        // return $card;
+
+        return view('cards.show', compact('card'));
+
+
+
+
+    }
+
 
 }
