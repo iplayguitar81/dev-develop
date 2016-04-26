9f4beacc5f8c8acc20914bf247b885695777ce76
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-26 00:28:52 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,30 +5,30 @@
     <h1>Create New Post</h1>
     <hr/>
 
-    {{--{!! Form::open(['url' => 'posts', 'class' => 'form-horizontal']) !!}--}}
+    {!! Form::open(['url' => 'posts', 'class' => 'form-horizontal']) !!}
 
-                {{--<div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">--}}
-                {{--{!! Form::label('title', 'Title: ', ['class' => 'col-sm-3 control-label']) !!}--}}
-                {{--<div class="col-sm-6">--}}
-                    {{--{!! Form::text('title', null, ['class' => 'form-control']) !!}--}}
-                    {{--{!! $errors->first('title', '<p class="help-block">:message</p>') !!}--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="form-group {{ $errors->has('body') ? 'has-error' : ''}}">--}}
-                {{--{!! Form::label('body', 'Body: ', ['class' => 'col-sm-3 control-label']) !!}--}}
-                {{--<div class="col-sm-6">--}}
-                    {{--{!! Form::textarea('body', null, ['class' => 'form-control']) !!}--}}
-                    {{--{!! $errors->first('body', '<p class="help-block">:message</p>') !!}--}}
-                {{--</div>--}}
-            {{--</div>--}}
+                <div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+                {!! Form::label('title', 'Title: ', ['class' => 'col-sm-3 control-label']) !!}
+                <div class="col-sm-6">
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                    {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
+            <div class="form-group {{ $errors->has('body') ? 'has-error' : ''}}">
+                {!! Form::label('body', 'Body: ', ['class' => 'col-sm-3 control-label']) !!}
+                <div class="col-sm-6">
+                    {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+                    {!! $errors->first('body', '<p class="help-block">:message</p>') !!}
+                </div>
+            </div>
 
 
-    {{--<div class="form-group">--}}
-        {{--<div class="col-sm-offset-3 col-sm-3">--}}
-            {{--{!! Form::submit('Create', ['class' => 'btn btn-primary form-control']) !!}--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--{!! Form::close() !!}--}}
+    <div class="form-group">
+        <div class="col-sm-offset-3 col-sm-3">
+            {!! Form::submit('Create', ['class' => 'btn btn-primary form-control']) !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
 
     @if ($errors->any())
         <ul class="alert alert-danger">
