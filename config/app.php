566ede5602e06cec5af5eab712bacf42c5e6579a
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-25 23:17:41 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- config/app.php
+++ config/app.php
@@ -155,8 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        //Appzcoder\CrudGenerator\CrudGeneratorServiceProvider::class,
-        //Collective\Html\HtmlServiceProvider::class,
+        Appzcoder\CrudGenerator\CrudGeneratorServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
