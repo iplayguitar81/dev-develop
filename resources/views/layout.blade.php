3f4a79fcba303c6a8a9cb16ae8b128c7a9743cbe
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-25 22:03:13 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,8 @@
 <html lang ="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bowtie Software &amp; Web Development</title>
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/1-col-portfolio.css">
