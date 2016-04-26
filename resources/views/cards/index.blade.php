9d8e8552711a3b71f0de1abcaaaeda04a5b9b939
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-26 01:05:19 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -5,9 +5,7 @@
 <h1>All Cards</h1>
 
     @foreach ($cards as $card)
-    <p><a href="/cards/{{$card->id}}">{{$card->title}}</a></p>
 
-    <p>{{$card->created_at}}</p>
 
     <!-- Project One -->
     <div class="row">
@@ -17,7 +15,7 @@
             </a>
         </div>
         <div class="col-md-5">
-            <h3>{{$card->title}}</h3>
+            <h3><a href="/cards/{{$card->id}}">{{$card->title}}</a></h3>
             <h4>{{$card->created_at}}</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
             <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
