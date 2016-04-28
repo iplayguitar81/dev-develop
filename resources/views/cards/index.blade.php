cdca2a5772652f39b4e11edf9da66a289c3a75b4
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-28 00:47:03 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -18,7 +18,7 @@
             <h3><a href="/cards/{{$card->id}}">{{$card->title}}</a></h3>
             <h4>{{$card->created_at}}</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-            <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <a class="btn btn-primary" href="/cards/{{$card->id}}">View Card <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
     </div>
 
