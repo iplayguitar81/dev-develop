9612784f6f732cf20d4e75404d0b91f6d7860636
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-28 18:05:35 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,6 +28,11 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+
+                <li>
+                    <a href="{{URL::to('/')}}">Home</a>
+                </li>
+
                 <li>
                     <a href="{{URL::to('/about')}}">About</a>
                 </li>
