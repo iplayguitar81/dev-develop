648ddb8e3aae0e2dd4d246ec463f7053b650596f
iplayguitar81 <iplayguitar81@yahoo.com>
2016-04-30 23:04:10 -0700

    Revert "get this stuff figured out"
    This reverts commit 43e2d84cc152b480f52b297ff1b70336a29c3611.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,8 +13,8 @@ class PagesController extends Controller
     public function home() {
 
         $data = array(
-            'home_posts'  => Post::all(),
-            'home_cards' => Card::all(),
+            'home_posts'  => PostsController::all(),
+            'home_cards' => CardsController::all(),
 
         );
 
